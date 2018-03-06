02cf1a810cd2d54ee0c1312f38fc479d75266771
Diogo Menezes <diogo.afroruts@gmail.com>
2018-03-06 11:06:21 -0300

    Criando Primeira Rota

--- src/Application.php
+++ src/Application.php
@@ -16,11 +16,6 @@ class Application
         $this->serviceContainer = $serviceContainer;
     }
 
-    public function service($name)
-    {
-        return $this->serviceContainer->get($name);
-    }
-
     public function addService(string $name, $service): void
     {
         if (is_callable($service)) {
@@ -33,5 +28,24 @@ class Application
     public function plugin(PluginInterface $plugin): void
     {
         $plugin->register($this->serviceContainer);
+    }
+
+    public function get($path, $action, $name = null): Application
+    {
+        $routing = $this->service('routing');
+        $routing->get($name, $path, $action);
+        return $this;
+    }
+
+    public function service($name)
+    {
+        return $this->serviceContainer->get($name);
+    }
+
+    public function start()
+    {
+        $route = $this->service('route');
+        $callable = $route->handler;
+        $callable();
     }
 }
