1dece528887715671748a6e123276ce333e5b24d
Diogo Menezes <diogo.afroruts@gmail.com>
2018-03-15 18:58:09 -0300

    Iniciando com repositories

--- src/Repository/DefaultRepository.php
+++ src/Repository/DefaultRepository.php
@@ -1,10 +1,7 @@
 <?php
 declare(strict_types=1);
 namespace SONFin\Repository;
-
-
 use Illuminate\Database\Eloquent\Model;
-
 class DefaultRepository implements RepositoryInterface
 {
     /**
@@ -15,7 +12,6 @@ class DefaultRepository implements RepositoryInterface
      * @var Model
      */
     private $model;
-
     /**
      * DefaultRepository constructor.
      * @param string $modelClass
@@ -25,7 +21,6 @@ class DefaultRepository implements RepositoryInterface
         $this->modelClass = $modelClass;
         $this->model = new $modelClass;
     }
-
     public function all(): array
     {
         // TODO: Implement all() method.
@@ -34,12 +29,10 @@ class DefaultRepository implements RepositoryInterface
     {
         // TODO: Implement create() method.
     }
-
     public function update(int $id, array $data)
     {
         // TODO: Implement update() method.
     }
-
     public function delete(int $id)
     {
         // TODO: Implement delete() method.
