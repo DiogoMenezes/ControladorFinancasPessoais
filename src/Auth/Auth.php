cc3c7eb58a556aaf31bfd2088b704cf708872ef5
Diogo Menezes <diogo.afroruts@gmail.com>
2018-03-26 11:47:12 -0300

    Iniciando login e serviço de autenticação

---
+++ src/Auth/Auth.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dgm
+ * Date: 3/26/2018
+ * Time: 11:35 AM
+ */
+
+namespace SONFin\Auth;
+
+
+class Auth implements AuthInterface
+{
+
+    public function login(array $credentials): bool
+    {
+        // TODO: Implement login() method.
+    }
+
+    public function check(): bool
+    {
+        // TODO: Implement check() method.
+    }
+
+    public function logout(): void
+    {
+        // TODO: Implement logout() method.
+    }
+}
